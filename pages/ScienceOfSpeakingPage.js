d4c34511ac2d09b1fa4e8e4e9d577782d0887130
fwan269 <fwan269@aucklanduni.ac.nz>
2025-10-02 13:40:36 +1300

    changed slideshow

--- pages/ScienceOfSpeakingPage.js
+++ pages/ScienceOfSpeakingPage.js
@@ -21,7 +21,8 @@ export default {
       <div class="row flex-grow-1 position-relative">
         <div class="col p-0">
           <iframe 
-            src="https://www.canva.com/design/DAGGkZnYEBo/4lT1haX4lenGwclLl2VdRA/view?embed" 
+            // src="https://www.canva.com/design/DAGGkZnYEBo/4lT1haX4lenGwclLl2VdRA/view?embed" 
+            src="https://www.canva.com/design/DAG0Zks6y6c/cGd90RTWpqJ_Rq5hc8oIqQ/view?utm_content=DAG0Zks6y6c&utm_campaign=designshare&utm_medium=link2&utm_source=uniquelinks&utlId=h146e7f6655" 
             style="width: 100%; height: 100%; margin: 0;" 
             allowfullscreen>
           </iframe>
